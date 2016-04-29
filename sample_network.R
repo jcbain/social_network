7240fadbcf3b09c257d968a9ac70ca43d0a08696
James Bain <jamescbain@gmail.com>
2016-04-28 21:54:37 -0500

    create a scale free network

--- sample_network.R
+++ sample_network.R
@@ -43,6 +43,7 @@ plot(bsk.network)
 require(igraph)
 # igraph with youtube data
 t<-read.csv('data/YouTube-dataset/data/edges.csv',header = FALSE)
+s<-t[sample(nrow(t),100),]
 net<-graph.data.frame(t, directed=T)
 V(net)
 E(net)
@@ -52,5 +53,40 @@ bad.vs<-V(net)[degree(net)<100]
 bsk.network<-delete.vertices(net, bad.vs)
 plot(bsk.network)
 
-g2 <- make_full_graph(4) + (make_full_graph(4) - path(1,2))
-predict_edges(g2)
+#https://www.r-project.org/conferences/useR-2008/slides/Csardi.pdf
+is.connected(net) 
+no.clusters(net)
+table(clusters(net)$csize)
+max(degree(net, mode="in")) 
+max(degree(net, mode="out")) 
+max(degree(net, mode="all"))
+plot(degree.distribution(net, mode="out"), log="xy")
+
+
+cl <- clusters(net)
+net2 <- subgraph(net, which(cl$membership == which.max(cl$csize)-1)-1)
+summary(net2)
+graph.density(net2) 
+g <- erdos.renyi.game(vcount(net), ecount(net), type="gnm")
+transitivity(g)
+transitivity(net)
+g2 <- degree.sequence.game(degree(net,mode="all"), method="vl")
+transitivity(g2)
+
+
+fc <- fastgreedy.community(simplify(as.undirected(net)))
+memb<- community.to.membership(net,fc$merges,whic.max(fc$modularity))
+lay <- layout.drl(net)
+net2 <- graph.empty(n=vcount(net))
+colbar <- rainbow(5)
+col <- colbar[memb$membership+1]
+col[is.na(col)] <- "grey"
+plot(net2, layout=lay, vertex.size=1,
+    vertex.label=NA, asp=FALSE,
+    vertex.color=col,
+    vertex.frame.color=col)
+
+# scale free network
+g <- barabasi.game(10000)
+#to look up
+# http://horicky.blogspot.com/2012/04/basic-graph-analytics-using-igraph.html
